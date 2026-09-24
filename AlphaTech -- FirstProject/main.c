b8112ad9033dc6f683811b42d7d98b5e2a13872a
Rıza Can <r.canyavuz18@gmail.com>
2026-09-24 17:40:44 +0300

    finish part8

--- AlphaTech -- FirstProject/main.c
+++ AlphaTech -- FirstProject/main.c
@@ -17,7 +17,7 @@ int main()
     return 0;
     */
     //format specification:
-
+/*
     printf("I am %d years old and next year I'm going to be %d years old.\n", 25, 26); // %d place holder just change place strings
     printf("I was almost fail the algorithm class with %.1f\n",44.6); // 44.60000 become 44.6 by using %.1f
     printf("We have %d coins in the bank\n",2000);
@@ -29,6 +29,40 @@ int main()
     printf("num1= %d, num2= %d, sub= %d\n",5,3,5-3);
     printf("a= %d, b= %d, sum= a+b= %d +%d = %d\n",3,5,3,5,3+5);
     printf("a= %d, b= %d, sum= %d + %d = %d\n", 3,5,3,5,3+5);
+    return 0;
+*/
+/*
+    printf("%d + %d = %d \n", 5, 2, 5+2);
+    printf("%d - %d = %d \n", 5, 2, 5-2);
+    printf("%d * %d = %d \n", 5, 2, 5*2);
+    printf("%d / %d = %d \n", 5, 2, 5/2);
+    printf("%d %% %d = %d \n", 5, 2, 5%2); //mod
+*/
+//exercise40:
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("**********\n""**********\n""**********\n""**********\n""**********\n");
+    printf("1*********\n");
+    printf("12********\n");
+    printf("123*******\n");
+    printf("1234******\n");
+    printf("12345*****\n");
+    printf("1*********\n""12********\n""123*******\n""1234******\n""12345*****\n");
+    //print x shape question
+    printf("*     *\n");
+    printf(" *   * \n");
+    printf("  * *  \n");
+    printf("   *   \n");
+    printf("  * *  \n");
+    printf(" *   * \n");
+    printf("*     *\n");
+    //print your birth year
+    printf("current year is : %d, year of my birth: %d\n and my age is sub = %d - %d = %d ",2026,2001,2026,2001,2026-2001);
 
     return 0;
 }
+
+
